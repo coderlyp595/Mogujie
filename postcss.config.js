5952f92ede90dffbab252bd09125a69bf948e1a8
coderlyp595 <507584973@qq.com>
2020-03-06 21:38:03 +0800

    购物车

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
-// module.exports = {
-//   plugins: {
-//     autoprefixer: {},
-//     'postcss-px-to-viewport': {
-//       viewportWidth: 375, // 视窗的宽度
-//       viewportHeight: 667, // 视窗的高度
-//       unitPrecision: 5, // 指定'px'转化为视窗单位值的小数位
-//       viewportUnit: 'vw', // 指定转换成视窗的单位，建议用'vw'
-//       selectorBlackList: ['ignore', 'tab-bar', 'tab-bar-item'], // 指定不需要转换的类
-//       minPixeValue: 1, // 小于或等于'1px'不能转化为视窗单位
-//       mediaQuery: false, // 允许媒体查询中转换'px'
-//       exclude: [/TabBar/]
-//     }
-//   }
-// }
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-px-to-viewport': {
+      viewportWidth: 375, // 视窗的宽度
+      viewportHeight: 667, // 视窗的高度
+      unitPrecision: 5, // 指定'px'转化为视窗单位值的小数位
+      viewportUnit: 'vw', // 指定转换成视窗的单位，建议用'vw'
+      selectorBlackList: ['ignore', 'tab-bar', 'tab-bar-item'], // 指定不需要转换的类
+      minPixeValue: 1, // 小于或等于'1px'不能转化为视窗单位
+      mediaQuery: false, // 允许媒体查询中转换'px'
+      exclude: [/TabBar/]
+    }
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,50 @@ module.exports = {
                 'network': '@/network',
                 'views': '@/views'
             }
-        }
+        },
+		// devServer: { port: 8080, disableHostCheck: true, },
+		// css: { 
+		// loaderOptions: { 
+		// 	postcss: { 
+		// 		plugins: [ require("postcss-px-to-viewport")({ unitToConvert: "px", viewportWidth: 750, unitPrecision: 3, propList: [ "*" ], viewportUnit: "vw", fontViewportUnit: "vw", selectorBlackList: [], minPixelValue: 1, mediaQuery: false, replace: true, exclude: /(\/|\\)(node_modules)(\/|\\)/, }) ]
+		// 		 } } }
     }
 }
+  // "eslintConfig": {
+  //   "root": true,
+  //   "env": {
+  //     "node": true
+  //   },
+  //   "extends": [
+  //     "plugin:vue/essential",
+  //     "eslint:recommended"
+  //   ],
+  //   "parserOptions": {
+  //     "parser": "babel-eslint"
+  //   }
+  // },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // module.exports = { 
 // 	devServer: { port: 8080, disableHostCheck: true, }, 
